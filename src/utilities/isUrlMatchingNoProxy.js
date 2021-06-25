065a63a85f70194637b5e6873c274e2592c17f19
Alex Hancock <alexhancock@squareup.com>
2021-06-25 11:06:59 -0700

    Add debug logging for NO_PROXY matching

--- src/utilities/isUrlMatchingNoProxy.js
+++ src/utilities/isUrlMatchingNoProxy.js
@@ -10,13 +10,16 @@ import {
 
 export default (subjectUrl: string, noProxy: string) => {
   const subjectUrlTokens = parseUrl(subjectUrl);
-
   const rules = noProxy.split(/[\s,]+/);
+
+  console.log('NO_PROXY_DEBUG', { subjectUrl, noProxy, subjectUrlTokens, rules });
 
   for (const rule of rules) {
     const ruleMatch = rule
       .replace(/^(?<leadingDot>\.)/, '*')
       .match(/^(?<hostname>.+?)(?::(?<port>\d+))?$/);
+
+    console.log('NO_PROXY_DEBUG', { ruleMatch });
 
     if (!ruleMatch || !ruleMatch.groups) {
       throw new UnexpectedStateError('Invalid NO_PROXY pattern.');
@@ -27,6 +30,8 @@ export default (subjectUrl: string, noProxy: string) => {
     }
 
     const hostnameIsMatch = matcher.isMatch(subjectUrlTokens.hostname, ruleMatch.groups.hostname);
+
+    console.log('NO_PROXY_DEBUG', { hostnameIsMatch, 1: !ruleMatch.groups, 2: !ruleMatch.groups.port, 3: subjectUrlTokens.port, 4: subjectUrlTokens.port === ruleMatch.groups.port });
 
     if (hostnameIsMatch && (!ruleMatch.groups || !ruleMatch.groups.port || subjectUrlTokens.port && subjectUrlTokens.port === ruleMatch.groups.port)) {
       return true;
